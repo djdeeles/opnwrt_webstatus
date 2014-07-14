29998af7d2fc1c01bcb74710aea6d53b77ceb83e
Çetin Çöne <djdeeles@gmail.com>
2014-07-14 11:01:06 +0300

    logreader: add data sorting

--- logreader.php
+++ logreader.php
@@ -3,14 +3,18 @@ $start =  microtime(true);
 require_once 'conn.php';
 require_once 'login.php';
 require('data.php');
-$per_page = 10; 
+$per_page = 15; 
 $logtype  = 0;
 $page     = 1;
+$sortby	  = "id";
+$sort 	  = "desc";
 
 if ($loggedin) {
 
 	if ($_GET['page']) { $page= $_GET['page']; }
 	if ($_GET['logtype']) { $logtype= $_GET['logtype']; }
+	if ($_GET['sortby']) { $sortby= $_GET['sortby']; }
+	if ($_GET['sort']) { $sort= $_GET['sort']; }
 	if (isset($_GET['log2db'])) { 
 	  	include('log2db.php');
 		header("Location: ". $_SERVER['HTTP_REFERER']);
@@ -18,50 +22,56 @@ if ($loggedin) {
 }
 
 function displaylogs() {
-	global $page,$logtype,$per_page;
+	global $page,$logtype,$per_page,$sortby,$sort;
 	//getting table contents
     $start  = ($page-1)*$per_page;
-    $result = mysql_query("select * from System_Logs where logtype=$logtype order by id desc limit $start,$per_page");
+    $result = mysql_query("select * from System_Logs where logtype=$logtype order by $sortby $sort limit $start,$per_page");
 
     if(mysql_num_rows($result) > 0) {
       while($row = mysql_fetch_array($result))
       {
         $id         = $row['id'];
         $log        = $row['log'];
-        $logdate  = $row['logdate'];
+        $logdate  	= $row['logdate'];
 
-        echo "
-        <tr>
-          <td>$id</td>
-          <td>$log</td>
-          <td>$logdate</td>
-        </tr>";
+        $logs 		.= "<tr>
+				          <td>$id</td>
+				          <td>$log</td>
+				          <td>$logdate</td>
+				        </tr>";
       } //End while
-    } else {
-      echo "<tr><td colspan='3'>No Result</td></tr>";
     }
+    echo $logs;
 }
 function pagination() {
-	global $page,$logtype,$per_page;
+	global $page,$logtype,$per_page,$sortby,$sort;
 	//getting number of rows and calculating no of pages
 	$result = mysql_query("select count(*) from System_Logs where logtype=$logtype");
 	$count  = mysql_fetch_row($result);
 	$pages  = ceil($count[0]/$per_page);
 	if ($pages > 1) {              
-	//show prev & first
-	if ($page > 1) { echo '<li class="prev"><a href="logreader.php?logtype='.$logtype.'&page=1">First</a></li>
-	                       <li class="prev"><a href="logreader.php?logtype='.$logtype.'&page='.($page -1).'">Prev</a></li>'; }
-	//Show page links
-	for($i=1; $i<=$pages; $i++)
-	{
-	  if( $i > ($page + 3) or $i < ($page - 3) ) { continue; }
-	  elseif ($i == $page ) { echo '<li class="active"><a href="#">'.$i.'</a></li>'; }
-	  else { echo '<li><a href="logreader.php?logtype='.$logtype.'&page='.$i.'">'.$i.'</a></li>'; }
+		//show prev & first
+		if ($page > 1) { $pagination = "<li class='prev'><a href='logreader.php?logtype=$logtype"."&page=1"."&sortby=$sortby"."&sort=$sort"."'>First</a></li>
+		                       			<li class='prev'><a href='logreader.php?logtype=$logtype"."&page=".($page -1)."&sortby=$sortby"."&sort=$sort"."'>«</a></li>"; }
+		//Show page links
+		for($i=1; $i<=$pages; $i++)
+		{
+		  if( $i > ($page + 3) or $i < ($page - 3) ) { continue; }
+		  elseif ($i == $page ) { $pagination .= '<li class="active"><a href="#">'.$i.'</a></li>'; }
+		  else { $pagination .= "<li><a href='logreader.php?logtype=$logtype"."&page=$i"."&sortby=$sortby"."&sort=$sort"."'>".$i."</a></li>"; }
+		}
+		//show next & last
+		if ($page < $pages) { $pagination .= "<li class='Next'><a href='logreader.php?logtype=$logtype"."&page=".($page +1)."&sortby=$sortby"."&sort=$sort"."'>»</a></li>
+		                            		  <li class='prev'><a href='logreader.php?logtype=$logtype"."&page=$pages"."&sortby=$sortby"."&sort=$sort"."'>Last(".$pages.")</a></li>"; }
 	}
-	//show next & last
-	if ($page < $pages) { echo '<li class="Next"><a href="logreader.php?logtype='.$logtype.'&page='.($page +1).'">Next</a></li>
-	                            <li class="prev"><a href="logreader.php?logtype='.$logtype.'&page='.$pages.'">Last('.$pages.')</a></li>'; }
-	}
+	echo $pagination;
+}
+function sortdata($asortby,$sortname) {
+	global $page,$logtype,$sort,$sortby;
+	if ($sort=="desc"){	$asort = "asc"; } elseif ($sort=="asc") { $asort="desc"; }	
+	if ($sortby==$asortby){ $headerstyle = "style='color:orange;'"; }
+	$header = "<a $headerstyle href='logreader.php?logtype=$logtype"."&page=$page"."&sortby=$asortby"."&sort=$asort'>$sortname"."</a>";
+	echo $header;
 }
 ?>
 
@@ -93,20 +103,25 @@ function pagination() {
       </select>    
     </div>    
     <a style="float:right;" href="<?php echo $_SERVER['PHP_SELF']; ?>?log2db" title="Refresh Logs" data-toggle="modal" class="btn btn-small">Refresh Logs</a>
+    <?php 	
+    	if(!$loggedin) { echo "<p style='float:left;width:100%;font-weight:bold;'>Please Login</p>"; }
+    	elseif($logtype != 0 && $loggedin) { 
+    ?>
     <table class='table logs'>
       <tr>
-        <th><b>Id</b></th>
-        <th><b>Log</b></th>
-        <th><b>Date</b></th>
+        <th width="4%"><?php sortdata("id","Id"); ?></th>
+        <th width="82%"><?php sortdata("log","Log"); ?></th>
+        <th width="14%"><?php sortdata("logdate","Date"); ?></th>
       </tr>
-      	<?php if($loggedin) { displaylogs(); } else { echo "<tr><td colspan='3'>Please Login</td></tr>"; } ?>
+      	<?php displaylogs(); ?>
     </table>
     <div class="pagination">  
       <ul>
-        <?php if($loggedin) { pagination(); } ?>
+      	<?php pagination(); ?>
       </ul> 
     </div>
-    <span><small><b>Page generated in</b> <?php echo round((microtime(true) - $start), 2); ?> seconds.</small></span>
+    <?php }  else { echo "<p style='float:left;width:100%;font-weight:bold;'>No Result</p>"; } ?>    
+    <p style="float:left;width:100%;"><small><b>Page generated in</b> <?php echo round((microtime(true) - $start), 2); ?> seconds.</small></p>
   </div>
 </body>
 </html>
