d1526889214af8b98e13b989f5202f13c155f985
Çetin Çöne <djdeeles@gmail.com>
2014-12-30 10:52:17 +0200

    vnstat: rework graphing method & add summary charts.
    additional visual fixes
    fix calculation of bytes in tickFormaY.

--- stats/index.php
+++ stats/index.php
@@ -21,11 +21,12 @@ $start =  microtime(true);
             	print "<a class=\"btn btn-default active\" href=\"$script\"><span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></a>";
         }
         foreach ($iface_list as $if)
-        {
+        {	
+        	$page = isset($_GET['page']) ? $_GET['page'] : 's';
             if ($iface == $if && !$defaultpage) {
-            	print "<a class=\"btn btn-default active\" href=\"$script?if=$if\">";
+            	print "<a class=\"btn btn-default active\" href=\"$script?if=$if&amp;page=$page\">";
             } else {
-            	print "<a class=\"btn btn-default\" href=\"$script?if=$if\">";
+            	print "<a class=\"btn btn-default\" href=\"$script?if=$if&amp;page=$page\">";
             }
             if (isset($iface_title[$if]))
             {
@@ -135,7 +136,7 @@ $start =  microtime(true);
         print "</table></div>\n";
     }
     
-    function write_graph_data($data)
+    function write_graph_data($data,$graph,$type)
     {
         $receivedData = [
             'className' => '.received',
@@ -145,13 +146,42 @@ $start =  microtime(true);
             'className' => '.sent',
             'data'      => [],
         ];
-        foreach ($data as $line) {                  
-            $receivedData['data'][] = ['x' => $line['img_label'], 'y' => $line['rx']*1024];
-            $sentData['data'][] = ['x' => $line['img_label'], 'y' => $line['tx']*1024];
+        foreach ($data as $line) {
+        	$x="";
+        	is_numeric($line['img_label']) ? $x+=$line['img_label'] : $x=$line['img_label'];
+            $receivedData['data'][] = ['x' => $x, 'y' => $line['rx']*1024];
+            $sentData['data'][] = ['x' => $x, 'y' => $line['tx']*1024];
         }
         $receivedData['data'] = array_reverse($receivedData['data']);
         $sentData['data'] = array_reverse($sentData['data']);
-        return json_encode($receivedData) . "," . json_encode($sentData); 
+        $graph_data = json_encode($receivedData) . "," . json_encode($sentData);
+        return print "<div class='graph-wrapper table-bordered'><figure class='graph' id='$graph'></figure></div> 
+    				   <script type='text/javascript'>
+			            var chart = new xChart(
+			                'bar',
+			                {
+			                    'xScale': 'ordinal', //ordinal,linear,time,exponential
+			                    'yScale': 'linear', //ordinal,linear,time,exponential
+			                    'type': '$type', //bar, cumulative,line,line-dotted
+			                    'main': [ $graph_data ]
+			                },
+			                '#$graph',
+			                {
+			                    'tickHintX': -25,
+			                    'paddingTop' : 5,
+			                    'paddingBottom' : 15,
+			                    'paddingLeft' : 60,
+			                    'paddingRight' : 0,
+			                    'tickFormatY': function (y) { 
+									var sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+								    if (y == 0) return '0 B';
+								    var i = parseInt(Math.floor(Math.log(y) / Math.log(1024)));
+								    if (i == 0) return bytes + ' ' + sizes[i]; 
+								    return (y / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+						        }
+			                }
+			            );
+			          </script>";
     }
 
     if (!$defaultpage) { 
@@ -171,9 +201,11 @@ $start =  microtime(true);
   <link href="css/vnstat.css" rel="stylesheet" type="text/css">
   <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="../css/custom.css" rel="stylesheet" type="text/css">
+  <link href="css/xcharts.min.css" rel="stylesheet" />
   <script src="../js/jquery-2.1.1.min.js" type="text/javascript"></script>
   <script src="../js/bootstrap.min.js" type="text/javascript"></script>
-  </style>
+  <script type="text/javascript" src="js/d3.min.js"></script>
+  <script type="text/javascript" src="js/xcharts.min.js"></script>
 </head>
 <body>
 	<div class="container vnstat">
@@ -188,27 +220,20 @@ $start =  microtime(true);
 			<?php                
 			if ($page == 's') {
                     write_summary(true);
-            } else { ?>
-            <div class="graph table-bordered">
-                <link href="css/xcharts.min.css" rel="stylesheet" />
-                <script type="text/javascript" src="js/d3.min.js"></script>
-                <script type="text/javascript" src="js/xcharts.min.js"></script>
-                <figure id="chart"></figure>
-            </div>            
-            <?php				
+            } else { 		
 				if ($page == 'h')
 				{
-					$graph_data = write_graph_data($hour);
+					write_graph_data($hour,"hour","line-dotted");
 					write_data_table(T('Last 24 hours'), $hour);
 				}
 				else if ($page == 'd')
 				{
-					$graph_data = write_graph_data($day);
+					$graph_data = write_graph_data($day,"day","line-dotted");
 					write_data_table(T('Last 30 days'), $day);
 				}
 				else if ($page == 'm')
 				{
-					$graph_data = write_graph_data($month);
+					$graph_data = write_graph_data($month,"month","line-dotted");
 					write_data_table(T('Last 12 months'), $month);
 				}
             }
@@ -216,6 +241,7 @@ $start =  microtime(true);
 			foreach ($iface_list as $iface) {
 				get_vnstat_data();
 				print "<h2><b>$iface_title[$iface]</b></h2>" ;
+				write_graph_data($month,$iface_title[$iface],"cumulative");
 				write_summary(false);
 			}
 	     } ?>
@@ -223,34 +249,6 @@ $start =  microtime(true);
 		<div class="row footer"><a href="http://www.cetincone.tk">aCC Stats 2.0</a><br/>
 			<small><b>Page generated in</b> <?php echo round((microtime(true) - $start), 2); ?> seconds.</small>
 		</div>
-        <?php if (isset($graph_data)) { ?>
-		<script type="text/javascript">
-            var chart = new xChart(
-                'bar',
-                {
-                    "xScale": "ordinal", //ordinal,linear,time,exponential
-                    "yScale": "linear", //ordinal,linear,time,exponential
-                    "type": "line-dotted", //bar, cumulative,line,line-dotted
-                    "main": [ <?php echo $graph_data; ?> ]
-                },
-                '#chart',
-                {
-                    "tickHintX": -24,
-                    "tickHintY": 8,
-                    "paddingTop" : "5",
-                    "paddingBottom" : "15",
-                    "paddingLeft" : "60",
-                    "paddingRight" : "0",
-                    "tickFormatY": function (y) {
-                        var units = ['B', 'KiB', 'MiB', 'GiB', 'TiB'];
-                        var pow   = Math.floor((y ? Math.log(y) : 0) / Math.log(1024));
-                        pow = Math.min(pow, units.length - 1);
-                        return (Math.round(y / (1 << (10 * pow)) * 10) / 10) + ' ' + units[pow];
-                    }
-                }
-            );
-        </script>        
-        <?php } ?>
 	</div>
 </body>
 </html>
