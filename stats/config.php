7d16fe455a17b9b1a5e27635bfd5101ea5e290e1
Çetin Ç <djdeeles@gmail.com>
2015-05-18 20:21:51 +0300

    enable wifi stats

--- stats/config.php
+++ stats/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    error_reporting(E_ALL | E_NOTICE);
+    error_reporting(0);
 
     $locale = 'tr_TR.UTF-8';
     $language = 'en';
@@ -10,7 +10,7 @@
     date_default_timezone_set("Europe/Istanbul");
 
     // list of network interfaces monitored by vnStat
-    $iface_list = array('eth0.2', 'br-lan');
+    $iface_list = array('eth0.2', 'br-lan', 'wlan0', 'wlan1');
 
     //
     // optional names for interfaces
@@ -19,6 +19,8 @@
     //
     $iface_title['eth0.2'] = 'Wan';
     $iface_title['br-lan'] = 'Lan';
+    $iface_title['wlan0'] = 'Wifi';
+    $iface_title['wlan1'] = 'Wifi 5G';
 
     $vnstat_bin = '/usr/bin/vnstat';
     $data_dir = './etc/vnstat';
